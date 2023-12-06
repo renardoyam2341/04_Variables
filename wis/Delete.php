2e19e53a5ed5abd10a120e551513f5edcec4a78f
renardoyam2341 <113009577+renardoyam2341@users.noreply.github.com>
2023-12-06 11:23:03 +0800

    Update Delete.php
    minor typo

--- wis/Delete.php
+++ wis/Delete.php
@@ -22,7 +22,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous">
-    <title>Update</title>
+    <title>Delete</title>
 </head>
 <body>
 
